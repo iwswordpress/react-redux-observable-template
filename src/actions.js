04319e5dba34f542f729a0dcf095b205f3a0d023
iwswordpress <iwswordpress@gmail.com>
2020-09-15 08:38:58 +0100

    changed a constant value to item

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,5 @@
 export const FETCH_ITEMS = 'FETCH_ITEMS';
-export const FETCH_ITEMS_FULFILLED = 'FETCH_TWEETS_FULFILLED';
+export const FETCH_ITEMS_FULFILLED = 'FETCH_ITEMS_FULFILLED';
 
 export const fetchItems = () => ({ type: FETCH_ITEMS });
 
